ab3ebde39420f58eb3142543c34ca09cf6f61511
Craig Giles <craig@gilesc.com>
2018-12-17 07:16:34 -0800

    New keybindings

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
-; NOTE: I am not a LISP programmer and I know next to nothing about the EMACS
-; ecosystem and programming environment. Coming from VIM, this could be
-; interesting...
+;; NOTE: I am not a LISP programmer and I know next to nothing about the EMACS
+;; ecosystem and programming environment. Coming from VIM, this could be
+;; interesting...
 
 ;; ===============================================================
 ;; Package Management
@@ -59,9 +59,11 @@
   ;; (define-key evil-normal-state-map (kbd "C-w C-k") (kbd "C-w k"))
   ;; (define-key evil-normal-state-map (kbd "C-w C-l") (kbd "C-w l"))
 
-  (define-key evil-normal-state-map (kbd "C-p") 'projectile--find-file)
+  (define-key evil-normal-state-map (kbd "C-p") 'find-file)
   (define-key evil-normal-state-map (kbd "C-f") 'ag-project-at-point)
-  (define-key evil-normal-state-map (kbd "C-l") 'other-window)
+
+  ;; NOTE(craig) -- i want to get in the habit of <C-w><C-w> to goto other window
+  ;; (define-key evil-normal-state-map (kbd "C-l") 'other-window)
   (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
   (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
   (define-key evil-insert-state-map (kbd "C-u")
@@ -75,7 +77,7 @@
     (global-evil-leader-mode)
     (evil-leader/set-leader "SPC")
     (evil-leader/set-key "n" 'evil-search-highlight-persist-remove-all)
-    (evil-leader/set-key "f" 'find-file)
+    (evil-leader/set-key "f" 'projectile--find-file)
     (evil-leader/set-key "o" 'projectile-find-file-other-window)
     (evil-leader/set-key "SPC" 'other-window)
     )
@@ -113,31 +115,23 @@
 (with-eval-after-load 'evil
   (defalias #'forward-evil-word #'forward-evil-symbol))
 
-;; (with-eval-after-load 'evil
-;;   (defadvice evil-inner-word (around underscore-as-word activate)
-;;     (let ((table (copy-syntax-table (syntax-table))))
-;;       (modify-syntax-entry ?_ "w" table)
-;;       (with-syntax-table table ad-do-it)))
-;; )
-
-;; Show matching paren
 (use-package autopair
   :config
   (show-paren-mode t)
-  (autopair-global-mode))
+  )
 
-;; (use-package smooth-scrolling
-;;   :config
-;;   (setq scroll-margin 8
-;;         scroll-conservatively 9999
-;;         scroll-step 1))
+(use-package smooth-scrolling
+  :config
+  (setq scroll-margin 8
+        scroll-conservatively 9999
+        scroll-step 1))
 
 (use-package fill-column-indicator
   :init
   (setq-default fill-column 80)
   (add-hook 'after-change-major-mode-hook 'fci-mode))
 
-; Ensure that emacs has the shell's PATH variables on osx
+;; Ensure that emacs has the shell's PATH variables on osx
 (use-package exec-path-from-shell
   :config
   (when (memq window-system '(mac ns x))
@@ -208,13 +202,10 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier nil)
 
-;; use spaces with 4 spaces per tab
-(setq-default tab-width 4 indent-tabs-mode nil)
-
 ;; Disable scroll bars and toolbars and welcome screen
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-; (setq inhibit-startup-screen t)
+;; (setq inhibit-startup-screen t)
 
 ;; Put the backup files in a single directory
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
@@ -230,28 +221,28 @@
       (append evil-emacs-state-modes evil-motion-state-modes))
 (setq evil-emacs-state-modes nil)
 
-; Stop Emacs from losing undo information by setting very high limits for undo
-; buffers
+;; Stop Emacs from losing undo information by setting very high limits for undo
+;; buffers
 (setq undo-limit 20000000)
 (setq undo-strong-limit 40000000)
 
-; Create a big horizontal blue bar so i don't keep loosing my cursor
+;; Create a big horizontal blue bar so i don't keep loosing my cursor
 (global-hl-line-mode 1)
 
-; Turn off the bell on Mac OS X
+;; Turn off the bell on Mac OS X
 (defun nil-bell ())
 (setq ring-bell-function 'nil-bell)
 
-; Move to the parent directory when in the dired directory listing
+;; Move to the parent directory when in the dired directory listing
 (define-key dired-mode-map "%" 'find-file)
-(define-key dired-mode-map "d" 'dired-create-directory)
-(define-key dired-mode-map "D" 'delete-file-or-directory)
+(define-key dired-mode-map "D" 'dired-create-directory)
+;; (define-key dired-mode-map "D" 'delete-file-or-directory)
 (define-key dired-mode-map "-"
-    (lambda ()
-      (interactive)
-      (find-alternate-file "..")))
+  (lambda ()
+    (interactive)
+    (find-alternate-file "..")))
 
-; Bright-red TODOs, NOTEs, and other things
+;; Bright-red TODOs, NOTEs, and other things
 (setq fixme-modes '(c++-mode c-mode emacs-lisp-mode scala-mode org-mode markdown-mode))
 (make-face 'font-lock-fixme-face)
 (make-face 'font-lock-study-face)
@@ -259,11 +250,11 @@
 (make-face 'font-lock-note-face)
 (mapc (lambda (mode)
         (font-lock-add-keywords
-          mode
-          '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
-            ("\\<\\(STUDY\\)" 1 'font-lock-study-face t)
-            ("\\<\\(IMPORTANT\\)" 1 'font-lock-important-face t)
-            ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
+         mode
+         '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
+           ("\\<\\(STUDY\\)" 1 'font-lock-study-face t)
+           ("\\<\\(IMPORTANT\\)" 1 'font-lock-important-face t)
+           ("\\<\\(NOTE\\)" 1 'font-lock-note-face t))))
       fixme-modes)
 (modify-face 'font-lock-fixme-face "Red" nil nil t nil t nil nil)
 (modify-face 'font-lock-study-face "Yellow" nil nil t nil t nil nil)
@@ -292,19 +283,25 @@
          ("\\.sbt$" . scala-mode)
          ) auto-mode-alist))
 
+;; Fix whitespace before saving to disc
+(add-hook 'before-save-hook
+          'fix-format-buffer)
 
-; Clock
+;; add global line numbers
+(global-linum-mode t)
+
+;; Clock
 (display-time)
 
 ;; ---------------------------------------------------------------
 
-; Theme based configuration
+;; Theme based configuration
 (load-theme 'zenburn t)
 ;; (load-theme 'solarized-dark t)
 
-; -----------------------------------------------
-; Old Theme Settings
-; -----------------------------------------------
+;; -----------------------------------------------
+;; Old Theme Settings
+;; -----------------------------------------------
 ;; (add-to-list 'default-frame-alist '(font . "Liberation Mono-12"))
 ;; (set-face-attribute 'default t :font "Liberation Mono-12")
 ;; (set-face-attribute 'font-lock-type-face nil :foreground "burlywood3")
@@ -330,21 +327,21 @@
 ;; (set-face-attribute 'lazy-highlight nil
 ;;                     :foreground "black"
 ;;                     :background "midnight blue")
-; -----------------------------------------------
-; I like but dont love
-; -----------------------------------------------
+;; -----------------------------------------------
+;; I like but dont love
+;; -----------------------------------------------
 (set-face-attribute 'font-lock-type-face nil :foreground "#dcdcdc")
 (set-face-attribute 'font-lock-variable-name-face nil :foreground "#dcdcdc")
 (set-face-attribute 'font-lock-constant-face nil :foreground "burlywood3")
 ;; (set-face-attribute 'font-lock-string-face nil :foreground "#5b845c")
 
-; -----------------------------------------------
-; Set in stone: Dont change
-; -----------------------------------------------
+;; -----------------------------------------------
+;; Set in stone: Dont change
+;; -----------------------------------------------
 (set-face-attribute 'default t :font "Liberation Mono-12")
 
-;; (set-background-color "#152426")               ; something akin to J.Blow's theme
-(set-face-background 'hl-line "midnight blue") ; the -always on- horizontal highlight
+;; (set-background-color "#152426")              ;; something akin to J.Blow's theme
+(set-face-background 'hl-line "midnight blue");; the -always on- horizontal highlight
 (set-foreground-color "#dcdcdc")
 (set-cursor-color "#40FF40")
 
@@ -354,15 +351,15 @@
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . fullheight))
 
-; Startup windowing
+;; Startup windowing
 (setq next-line-add-newlines nil)
 (setq-default truncate-lines t)
 (setq truncate-partial-width-windows nil)
 (split-window-horizontally)
 
 (defun never-split-a-window ()
-    "Don't want to attempt to split windows if i dont have to"
-    nil)
+  "Don't want to attempt to split windows if i dont have to"
+  nil)
 (setq split-window-preferred-function 'never-split-a-window)
 
 ;; ===============================================================
@@ -381,33 +378,33 @@
 ;; C++ Mode Configuration
 ;; ---------------------------------------------------------------
 (defun save-buffers-without-asking ()
-(interactive)
+  (interactive)
   (save-some-buffers 'no-confirm (lambda ()
-    (cond
-      ((and buffer-file-name (equal buffer-file-name abbrev-file-name)))
-      ((and buffer-file-name (eq major-mode 'markdown-mode)))
-      ((and buffer-file-name (eq major-mode 'c-mode)))
-      ((and buffer-file-name (eq major-mode 'cc-mode)))
-      ((and buffer-file-name (eq major-mode 'c++-mode)))
-      ((and buffer-file-name (eq major-mode 'scala-mode)))
-      ((and buffer-file-name (eq major-mode 'emacs-lisp-mode)))
-      ((and buffer-file-name (derived-mode-p 'org-mode)))))))
+                                   (cond
+                                    ((and buffer-file-name (equal buffer-file-name abbrev-file-name)))
+                                    ((and buffer-file-name (eq major-mode 'markdown-mode)))
+                                    ((and buffer-file-name (eq major-mode 'c-mode)))
+                                    ((and buffer-file-name (eq major-mode 'cc-mode)))
+                                    ((and buffer-file-name (eq major-mode 'c++-mode)))
+                                    ((and buffer-file-name (eq major-mode 'scala-mode)))
+                                    ((and buffer-file-name (eq major-mode 'emacs-lisp-mode)))
+                                    ((and buffer-file-name (derived-mode-p 'org-mode)))))))
 
 (setq compilation-directory-locked nil)
 (setq makescript-file "build.sh")
 
-; Compile Settings
+;; Compile Settings
 (setq compilation-context-lines 0)
 (setq compilation-error-regexp-alist
-    (cons '("^\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:fatal error\\|warnin\\(g\\)\\) C[0-9]+:" 2 3 nil (4))
-     compilation-error-regexp-alist))
+      (cons '("^\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:fatal error\\|warnin\\(g\\)\\) C[0-9]+:" 2 3 nil (4))
+            compilation-error-regexp-alist))
 
 (defun find-project-directory-recursive ()
   "Recursively search for a makefile."
   (interactive)
   (if (file-exists-p makescript-file) t
-      (cd "../")
-      (find-project-directory-recursive)))
+    (cd "../")
+    (find-project-directory-recursive)))
 
 (defun lock-compilation-directory ()
   "The compilation process should NOT hunt for a makefile"
@@ -427,9 +424,9 @@
   (setq find-project-from-directory default-directory)
   (switch-to-buffer-other-window "*compilation*")
   (if compilation-directory-locked (cd last-compilation-directory)
-  (cd find-project-from-directory)
-  (find-project-directory-recursive)
-  (setq last-compilation-directory default-directory)))
+    (cd find-project-from-directory)
+    (find-project-directory-recursive)
+    (setq last-compilation-directory default-directory)))
 
 (defun make-without-asking ()
   "Make the current build."
@@ -438,13 +435,13 @@
   (if (find-project-directory) (compile (concat "./" makescript-file)))
   (other-window 1))
 
-; Add header files to C++ mode
+;; Add header files to C++ mode
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 ;; ---------------------------------------------------------------
 
 
-; C++ indentation style
+;; C++ indentation style
 (defconst craigs-big-fun-c-style
   '((c-electric-pound-behavior   . nil)
     (c-tab-always-indent         . t)
@@ -487,53 +484,53 @@
                                     (brace-list-open       .  0)
                                     (brace-list-intro      .  4)))
     (c-echo-syntactic-information-p . t))
-    "Craigs's Big Fun C++ Style")
+  "Craigs's Big Fun C++ Style")
 
-; CC++ mode handling
+;; CC++ mode handling
 (defun craigs-big-fun-c-hook ()
-  ; Set my style for the current buffer
+  ;; Set my style for the current buffer
   (c-add-style "BigFun" craigs-big-fun-c-style t)
-  
-  ; 4-space tabs
+
+  ;; 4-space tabs
   (setq tab-width 4
         indent-tabs-mode nil)
 
-  ; Additional style stuff
+  ;; Additional style stuff
   (c-set-offset 'member-init-intro '++)
 
-  ; No hungry backspace
+  ;; No hungry backspace
   (c-toggle-auto-hungry-state -1)
 
-  ; Newline indents, semi-colon doesn't
+  ;; Newline indents, semi-colon doesn't
   (define-key c++-mode-map "\C-m" 'newline-and-indent)
   (setq c-hanging-semi&comma-criteria '((lambda () 'stop)))
 
-  ; Handle super-tabbify (TAB completes, shift-TAB actually tabs)
+  ;; Handle super-tabbify (TAB completes, shift-TAB actually tabs)
   (setq dabbrev-case-replace t)
   (setq dabbrev-case-fold-search t)
   (setq dabbrev-upcase-means-case-search t)
 
-  ; Abbrevation expansion
+  ;; Abbrevation expansion
   (abbrev-mode 1)
- 
+
   (defun craigs-find-corresponding-file ()
     "Find the file that corresponds to this one."
     (interactive)
     (setq CorrespondingFileName nil)
     (setq BaseFileName (file-name-sans-extension buffer-file-name))
     (if (string-match "\\.c" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".h")))
+        (setq CorrespondingFileName (concat BaseFileName ".h")))
     (if (string-match "\\.h" buffer-file-name)
-       (if (file-exists-p (concat BaseFileName ".c")) (setq CorrespondingFileName (concat BaseFileName ".c"))
-	   (setq CorrespondingFileName (concat BaseFileName ".cpp"))))
+        (if (file-exists-p (concat BaseFileName ".c")) (setq CorrespondingFileName (concat BaseFileName ".c"))
+          (setq CorrespondingFileName (concat BaseFileName ".cpp"))))
     (if (string-match "\\.hin" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".cin")))
+        (setq CorrespondingFileName (concat BaseFileName ".cin")))
     (if (string-match "\\.cin" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".hin")))
+        (setq CorrespondingFileName (concat BaseFileName ".hin")))
     (if (string-match "\\.cpp" buffer-file-name)
-       (setq CorrespondingFileName (concat BaseFileName ".h")))
+        (setq CorrespondingFileName (concat BaseFileName ".h")))
     (if CorrespondingFileName (find-file CorrespondingFileName)
-       (error "Unable to find a corresponding file")))
+      (error "Unable to find a corresponding file")))
   (defun craigs-find-corresponding-file-other-window ()
     "Find the file that corresponds to this one."
     (interactive)
@@ -545,7 +542,7 @@
   (define-key c++-mode-map [M-f12] 'craigs-find-corresponding-file-other-window)
   (define-key c++-mode-map [f5] 'make-without-asking)
 
-                                        ; devenv.com error parsing
+  ;; devenv.com error parsing
   (add-to-list 'compilation-error-regexp-alist 'craigs-devenv)
   (add-to-list 'compilation-error-regexp-alist-alist '(craigs-devenv
                                                        "*\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(\t\n]+\\)(\\([0-9]+\\)) : \\(?:see declaration\\|\\(?:warnin\\(g\\)\\|[a-z ]+\\) C[0-9]+:\\)"
@@ -596,7 +593,7 @@
   (interactive "sReplace: \nsReplace: %s  With: ")
   (save-excursion
     (replace-string FromString ToString)
-  ))
+    ))
 (define-key global-map [f8] 'replace-string-in-place)
 
 (add-hook 'c-mode-common-hook 'craigs-big-fun-c-hook)
