5ed5a823cf914a1712c225f3c951a47a9d88c76b
Craig Giles <craig@gilesc.com>
2019-03-13 20:17:02 -0700

    Changing background color

--- init.el
+++ init.el
@@ -756,7 +756,7 @@
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (set-foreground-color "burlywood3")
-  (set-background-color "#161616")
+  ;; (set-background-color "#161616")
   (set-cursor-color "#40FF40")
 )
 
