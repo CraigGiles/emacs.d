536eb8bb8068574986af7f67c017480d3b97f0f7
Craig Giles <craig@gilesc.com>
2019-03-14 06:53:44 -0700

    Creating a new init file

---
+++ init-test.el
@@ -0,0 +1,205 @@
+;; NOTE: I am not a LISP programmer and I know next to nothing about the EMACS
+;; ecosystem and programming environment. Coming from VIM, this could be
+;; interesting...
+
+;; ===============================================================
+;; Package Management
+;; ---------------------------------------------------------------
+(require 'package)
+
+;; find packages in melpa, melpa stable, or for org mode, orgmode over elpa (or
+;; marmalade for that matter)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(setq package-archive-priorities
+      '(("melpa-stable" . 1)
+        ("org" . 2)))
+
+(package-initialize)
+
+;; we're going to be using use-package to define and load packages.
+;; it's a code based configuration with a bunch of fancy extensions
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; ===============================================================
+;; Installed Packages
+;; ---------------------------------------------------------------
+(use-package counsel-projectile
+  :pin melpa-stable
+  :config
+  (counsel-mode))
+
+(use-package evil-commentary
+  :pin melpa-stable
+  :init
+  (evil-commentary-mode t))
+
+(use-package evil
+  :pin melpa-stable
+  :init
+  (evil-mode t)
+
+  :config
+  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+
+  (define-key evil-normal-state-map (kbd "-") 'find-file)
+
+  (define-key evil-normal-state-map (kbd "C-p") 'projectile--find-file)
+  (define-key evil-normal-state-map (kbd "C-f") 'ag-project-at-point)
+
+  ;; NOTE(craig) -- i want to get in the habit of <C-w><C-w> to goto other window
+  (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-insert-state-map (kbd "C-u")
+    (lambda ()
+      (interactive)
+      (evil-delete (point-at-bol) (point))))
+
+  (use-package evil-leader
+    :pin melpa-stable
+    :config
+    (setq evil-leader/in-all-states 1)
+    (global-evil-leader-mode)
+    (evil-leader/set-leader "SPC")
+    (evil-leader/set-key "n" 'evil-search-highlight-persist-remove-all)
+    (evil-leader/set-key "SPC" 'other-window))
+
+  (use-package evil-surround
+    :pin melpa-stable
+    :config
+    (global-evil-surround-mode))
+
+  (use-package evil-indent-textobject
+    :pin melpa-stable)
+
+  (use-package evil-search-highlight-persist
+    :pin melpa-stable
+    :config
+    (global-evil-search-highlight-persist t))
+
+  (use-package use-package-chords
+    :pin melpa-stable
+    :config
+    (key-chord-mode 1)
+    (setq key-chord-two-keys-delay 0.2)
+    ;; Exit insert mode with 'jj'
+    (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+    (key-chord-define evil-insert-state-map "Jj" 'evil-normal-state)
+    (key-chord-define evil-insert-state-map "JJ" 'evil-normal-state)
+    ;; (key-chord-define evil-normal-state-map "ls" 'buffer-menu)
+
+    ;; TODO(craig) -- move to scala-init.el for scala development
+    ;; Use ensime to get the type at the point
+    (key-chord-define evil-normal-state-map "et" 'ensime-type-at-point)
+    (key-chord-define evil-normal-state-map "ed" 'ensime-edit-definition-other-window)
+    )
+
+  )
+
+(use-package autopair
+  :pin melpa-stable
+  :config
+  (show-paren-mode t))
+
+(use-package smooth-scrolling
+  :pin melpa-stable
+  :config
+  (setq scroll-margin 8
+        scroll-conservatively 9999
+        scroll-step 1))
+
+(use-package fill-column-indicator
+  :pin melpa-stable
+  :init
+  (setq-default fill-column 80)
+  (add-hook 'after-change-major-mode-hook 'fci-mode))
+
+(with-eval-after-load 'evil
+  (defalias #'forward-evil-word #'forward-evil-symbol))
+
+(use-package make-mode
+  :pin melpa-stable
+  :config
+  (remove-hook 'before-save-hook 'fix-format-buffer t))
+
+(use-package cc-mode)
+(use-package compile)
+(use-package ido)
+
+(use-package 2048-game)
+
+;; ===============================================================
+;; General Editor Settings
+;; ---------------------------------------------------------------
+(setq emacs-version-osx (string-equal system-type "darwin"))
+(setq emacs-version-linux (string-equal system-type "gnu/linux"))
+(setq emacs-version-windows (string-equal system-type "windows-nt"))
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; Put all the backup files in an ~/.emacs.d/backup dir
+(setq backup-directory-alist '(("." . "~/.emacs.d/auto-saves")))
+(setq create-lockfiles nil)
+(setq auto-save-default nil) ;; IMPORTANT(craig) - remove this if it becomes necessary
+
+;; (setq next-line-add-newlines nil)
+(setq-default truncate-lines t)
+(setq truncate-partial-width-windows nil)
+
+;; Stop Emacs from losing undo information by setting very high limits for undo
+;; buffers
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
+
+;; Set the OSX's CMD key as the meta key
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; Create a big horizontal blue bar so i don't keep loosing my cursor
+(global-hl-line-mode 1)
+
+;; ===============================================================
+;; Theme Settings
+;; ---------------------------------------------------------------
+(add-to-list 'default-frame-alist '(font . "Liberation Mono-12"))
+(set-face-attribute 'default t :font "Liberation Mono-12")
+(set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
+(set-face-attribute 'font-lock-comment-face nil :foreground "gray50")
+(set-face-attribute 'font-lock-constant-face nil :foreground "olive drab")
+(set-face-attribute 'font-lock-doc-face nil :foreground "gray50")
+(set-face-attribute 'font-lock-function-name-face nil :foreground "white")
+(set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3")
+(set-face-attribute 'font-lock-string-face nil :foreground "olive drab")
+(set-face-attribute 'font-lock-type-face nil :foreground "#dcdcdc")
+(set-face-attribute 'font-lock-variable-name-face nil :foreground "#dcdcdc")
+
+(set-face-background 'hl-line "midnight blue");; the -always on- horizontal highlight
+(set-background-color "#152426")              ;; something akin to J.Blow's theme
+(set-foreground-color "#dcdcdc")
+(set-cursor-color "#40FF40")
+
+(set-face-attribute 'mode-line nil
+                    :background "burlywood3"
+                    :foreground "black")
+
+(defun post-load-stuff ()
+  (interactive)
+  "Load all the things that I want loaded, AFTER emacs is up and running"
+  (menu-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (toggle-frame-maximized)
+  ;; (exec-path-from-shell-initialize)
+  (load-file "~/.emacs.d/local-init.el")
+)
+(add-hook 'window-setup-hook 'post-load-stuff t)
