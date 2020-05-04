80382d2fbdfdf497ea5ef7a6e3b09ea712a6a199
Craig Giles <craig@gilesc.com>
2020-05-04 13:22:10 -0700

    changing font... again

--- init.el
+++ init.el
@@ -600,7 +600,8 @@
 ;; (add-to-list 'default-frame-alist '(width . 165))
 ;; (add-to-list 'default-frame-alist '(height . 60))
 ;; (set-face-attribute 'default t :font "Liberation Mono-24")
-(set-frame-font "Liberation Mono-16" nil t)
+;; (set-frame-font "Liberation Mono-16" nil t)
+(set-frame-font "Liberation Mono-12" nil t)
 
 ;; Colors
 (setq foreground-font-color                                       "#D2CBC0")
