9ab06daaf6dddfe015057a6ee17e043d39781183
Craig Giles <craig@gilesc.com>
2022-01-26 08:48:37 -0800

    Untabify for everything except makefiles

--- init.el
+++ init.el
@@ -6,8 +6,14 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
+
+(defun untabify-except-makefiles ()
+  "Replace tabs with spaces except in makefiles."
+  (unless (derived-mode-p 'makefile-mode)
+    (untabify (point-min) (point-max))))
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-;; (add-hook 'before-save-hook 'untabify)
+(add-hook 'before-save-hook 'untabify-except-makefiles)
 
 ;; Special values:
 ;;   `gnu'         compiled for a GNU Hurd system.
