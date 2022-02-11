c6d1e11d6a98337c9231a1cde620144bed399b28
Craig Giles <craig@gilesc.com>
2022-02-10 20:01:53 -0800

    Adding jai mode

--- init.el
+++ init.el
@@ -78,6 +78,7 @@
 ;; ---------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (load "kotlin-mode")
+(load "jai-mode")
 
 (use-package evil-commentary
   :pin melpa
@@ -265,6 +266,26 @@
     (add-hook 'before-save-hook 'gofmt-before-save)
 
 ) ;; go-mode
+
+;;   --- Jai Mode ---
+;; ---------------------------------------------------------------
+(defun my-jai-mode-hook ()
+    (add-to-list 'auto-mode-alist '("\\.kt$" . jai-mode))
+    (add-to-list 'fixme-modes 'jai-mode)
+    (initialize-fixme-modes)
+
+    (setq tab-stop 4)
+    (setq indent-tabs-mode nil)
+
+    (define-key jai-mode-map "\em" 'make-without-asking)
+    (define-key jai-mode-map (kbd "C-M-m") 'test-without-asking)
+
+    (setq build-file-name "build.bat")
+    (setq compile-command "call build.bat")
+    (message "Jai hook added")
+)
+
+(add-hook 'jai-mode-hook 'my-jai-mode-hook)
 
 ;;   --- Kotlin Mode ---
 ;; ---------------------------------------------------------------
