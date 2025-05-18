80b326724bfa6bbeab4e749b72844a344b586275
Craig Giles <craig@gilesc.com>
2025-05-18 12:15:48 -0700

    Font scaling with CTRL - and +

--- init.el
+++ init.el
@@ -291,3 +291,11 @@
 ;;
 ;;      -- Required Modes --
 ;; -----------------------------------------------------------------
+
+;;
+;;      -- Font Scaling --
+;; -----------------------------------------------------------------
+;; Add font scaling with CTRL+= and CTRL+-
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-0") 'text-scale-adjust) ;; Reset font size
